20bec5f605aa7419ea52491bee773ceb1e0ed4b4
Erick Fleming <efleming969@gmail.com>
2018-02-05 08:43:52 -0600

    exporting DOM for simple UI changes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import * as HTML from "./HTML"
 import * as HTTP from "./HTTP"
 import * as Lymph from "./Lymph"
 import * as Command from "./Command"
+import * as DOM from "./DOM"
 
 export {
-    Utils, HTML, HTTP, Lymph, Command
+    Utils, HTML, HTTP, Lymph, Command, DOM
 }
